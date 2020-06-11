ab8da34049a75e83ce7654ce01514eb2d159bd0d
Sarah Ali <asarahali00@gmail.com>
2020-06-11 15:33:49 -0500

    update HTTP port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Listening on Port ${port}`);
