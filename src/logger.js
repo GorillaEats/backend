2e7eb0e5c25b976bb10f0ee23c5aa7ce2875b7b0
Kevo1ution <kevin26428@gmail.com>
2020-06-12 00:29:19 -0500

    added formatting to console output

--- src/logger.js
+++ src/logger.js
@@ -1,13 +1,23 @@
-const winston = require('winston');
-
+const { createLogger, format, transports } = require('winston');
 const { LoggingWinston } = require('@google-cloud/logging-winston');
 
-const logger = winston.createLogger({
+const consoleOptions = {
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.printf(({
+      level, message, timestamp,
+    }) => `${timestamp.replace('T', ' ')} ${level}: ${message}`),
+  ),
+};
+
+const logger = createLogger({
   level: process.env.LOG_LEVEL,
+  defaultMeta: { service: 'backend' },
   transports: [
     process.env.NODE_ENV === 'production'
       ? new LoggingWinston()
-      : new winston.transports.Console(),
+      : new transports.Console(consoleOptions),
   ],
 });
 
