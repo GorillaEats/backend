1c34d6f8ce5767b236509e55d09eb39c3e528035
Kevin Tang <kevin26428@gmail.com>
2020-06-13 15:40:58 -0500

    [Gor 21] Setting up default error handler middleware (#8)
    * default error handler
    
    * using error handler middleware in express app
    
    * renaming folder
    
    * renaming folder
    
    * using absolute paths
    
    * adding absolute paths
    
    * finished test casess for error handling

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+require('app-module-path').addPath(__dirname);
 
-const logger = require('./src/logger');
+const logger = require('src/logger');
+const { errorHandler } = require('src/api/middleware');
 
 const app = express();
 
 const port = process.env.PORT || 8080;
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   logger.info(`Listening on Port ${port}`);
