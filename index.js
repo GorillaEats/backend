6d66a7269c6fac31f7657c6b3fe6da6d345f6cea
Kevo1ution <kevin26428@gmail.com>
2020-06-13 13:21:31 -0500

    using error handler middleware in express app

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const logger = require('./src/logger');
+const { errorHandler } = require('./src/api/middlewares');
 
 const app = express();
 
 const port = process.env.PORT || 8080;
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   logger.info(`Listening on Port ${port}`);
