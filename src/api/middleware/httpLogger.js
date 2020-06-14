e062b9e3dad2c9788d68925a9cccc6d1839bb675
Sarah Ali <asarahali00@gmail.com>
2020-06-14 13:54:31 -0500

    remove excess import from httpLogger

--- src/api/middleware/httpLogger.js
+++ src/api/middleware/httpLogger.js
@@ -1,5 +1,4 @@
 const morgan = require('morgan');
 const logger = require('../../logger');
-require('../../logger');
 
 module.exports = morgan(':method :url :status :response-time ms', { stream: logger.stream });
