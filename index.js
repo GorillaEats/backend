47f42e74b9f6f56b12171b3de92736bb3836e5e3
Sarah Ali <asarahali00@gmail.com>
2020-06-14 13:51:46 -0500

    add httpLogger middleware to index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 require('app-module-path').addPath(__dirname);
 
 const logger = require('src/logger');
-const { errorHandler } = require('src/api/middleware');
+const { errorHandler, httpLogger } = require('src/api/middleware');
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
 app.use(errorHandler);
+app.use(httpLogger);
 
 app.listen(port, () => {
   logger.info(`Listening on Port ${port}`);
