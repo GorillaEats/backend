f63074ec5a8dc14738f0adfdce631243c4b63312
Sarah Ali <asarahali00@gmail.com>
2020-06-14 20:14:16 -0500

    update statusCode const in httpLogger

--- test/src/api/middleware/httpLogger.js
+++ test/src/api/middleware/httpLogger.js
@@ -10,14 +10,15 @@ const logger = require('src/logger');
 test('morgan', async (t) => {
   const METHOD = 'GET';
   const PATH = '/';
-  const STATUS = '404';
 
   const loggerstub = sinon.stub(logger, 'info');
   const app = express();
   app.use(httpLogger);
-  await request(app)
-    .get('/');
+  const response = await request(app)
+    .get(PATH);
+
+  const statusCode = response.res.statusCode.toString();
 
   const args = loggerstub.getCall(0).firstArg.split(' ');
-  t.true(args[0] === METHOD && args[1] === PATH && args[2] === STATUS);
+  t.true(args[0] === METHOD && args[1] === PATH && args[2] === statusCode);
 });
