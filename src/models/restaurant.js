4a01676b050821df89dd4437bf1f9f5ebddc5c26
Kevin Tang <kevin26428@gmail.com>
2020-06-15 16:55:04 -0500

    [Gor 12] Restaurant Schema (#12)
    * added initial restuarant schema
    
    * fixed model lowercase bug
    
    * adding test for vegan rating value increments
    
    * removed unecessary messages
    
    * simplified validate fields
    
    * added virtual property to calculate veganRating
    
    * upper case
    
    * moved spider info to not required and in subdocument

---
+++ src/models/restaurant.js
@@ -0,0 +1,68 @@
+const { model, Schema } = require('mongoose');
+const validator = require('validator');
+
+const schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
+  },
+  spider: {
+    url: {
+      type: String,
+      index: true,
+      validate: validator.isURL,
+    },
+    allowedDomain: {
+      type: String,
+      index: true,
+      validate: validator.isURL,
+    },
+    allow: String,
+  },
+  reviewMeta: {
+    veganRatingTotal: {
+      type: Number,
+      default: 0,
+      min: 0,
+      validate: Number.isInteger,
+    },
+    veganRatingCount: {
+      type: Number,
+      default: 0,
+      min: 0,
+      validate: Number.isInteger,
+    },
+  },
+  locationCount: {
+    type: Number,
+    default: 0,
+    min: 0,
+    validate: Number.isInteger,
+  },
+});
+
+schema.index({ name: 'text' });
+
+schema.statics.updateVeganRating = function updateVeganRating(_id, ratingTotal, ratingCount) {
+  return this.updateOne(
+    { _id },
+    {
+      $inc: {
+        'reviewMeta.veganRatingTotal': ratingTotal,
+        'reviewMeta.veganRatingCount': ratingCount,
+      },
+    },
+  );
+};
+
+schema.virtual('reviewMeta.veganRating').get(function getVeganRating() {
+  if (this.reviewMeta.veganRatingCount === 0) {
+    return 0;
+  }
+
+  return this.reviewMeta.veganRatingTotal / this.reviewMeta.veganRatingCount;
+});
+
+module.exports = model('Restaurants', schema);
---
+++ test/models/restaurant.js
@@ -0,0 +1,68 @@
+const test = require('ava');
+
+require('../util/absolutePath');
+const { setupMongodbConnection, closeMongodbConnection } = require('test/util/mongoose');
+const Restaurant = require('src/models/restaurant');
+
+test.beforeEach(async (t) => {
+  const mongod = await setupMongodbConnection();
+
+  const restaurant = new Restaurant({
+    name: 'Chipotle',
+    spider: {
+      url: 'https://locations.chipotle.com/',
+      allowedDomain: 'locations.chipotle.com',
+      allow: '(/|(/[a-z]{2}(|\\.html|(/[a-zA-Z]+(?:[-][a-zA-Z]+)*(|\\.html|/[a-zA-Z0-9-]+(|\\.html))))))$',
+    },
+  });
+  await restaurant.save();
+
+  // eslint-disable-next-line no-param-reassign
+  t.context = {
+    fixture: {
+      restaurant,
+    },
+    mongod,
+  };
+});
+
+test.afterEach(async (t) => {
+  await closeMongodbConnection(t.context.mongod);
+});
+
+test('Static method updateVeganRating should properly increment vegan rating values', async (t) => {
+  const { restaurant } = t.context.fixture;
+  const rating = 2;
+  const ratingCount = 1;
+
+  await Restaurant.updateVeganRating(restaurant.id, rating, ratingCount);
+
+  const newRestaurant = await Restaurant.findOne({ _id: restaurant.id });
+
+  t.is(
+    restaurant.reviewMeta.veganRatingTotal + rating,
+    newRestaurant.reviewMeta.veganRatingTotal,
+  );
+  t.is(
+    restaurant.reviewMeta.veganRatingCount + ratingCount,
+    newRestaurant.reviewMeta.veganRatingCount,
+  );
+});
+
+test('Virtual property reviewMeta.veganRating should return 0 if no vegan ratings', async (t) => {
+  const { restaurant } = t.context.fixture;
+
+  t.is(restaurant.reviewMeta.veganRating, 0);
+});
+
+test('Virtual property reviewMeta.veganRating should give average', async (t) => {
+  const { restaurant } = t.context.fixture;
+  const total = 20;
+  const count = 5;
+  const average = total / count;
+
+  restaurant.reviewMeta.veganRatingTotal = total;
+  restaurant.reviewMeta.veganRatingCount = count;
+
+  t.is(restaurant.reviewMeta.veganRating, average);
+});
