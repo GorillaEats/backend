f2e9942b0a62847a7537d7ee21d5a9e1583196d0
Kevo1ution <kevin26428@gmail.com>
2020-06-16 00:47:48 -0500

    created menu schema

---
+++ src/models/menu.js
@@ -0,0 +1,12 @@
+const { model, Schema } = require('mongoose');
+
+const schema = new Schema({
+  items: [{
+    name: { type: String, required: true },
+    modifications: { type: [String], required: true },
+    tags: { type: [String], required: true },
+  }],
+  source: { type: String, required: true },
+});
+
+module.exports = model('Menus', schema);
