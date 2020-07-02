9784730590dc1da06dbef700df9d2153e4d75abe
Kevo1ution <kevin26428@gmail.com>
2020-07-01 21:43:32 -0500

    added reviewMeta property to a location

--- src/models/location.js
+++ src/models/location.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
+const Restaurant = require('./restaurant');
+
 const { model, Schema } = mongoose;
 
 const schema = new Schema({
@@ -49,6 +51,20 @@ const schema = new Schema({
     index: true,
     ref: 'Restaurant',
   },
+  reviewMeta: {
+    veganRatingTotal: {
+      type: Number,
+      default: 0,
+      min: 0,
+      validate: Number.isInteger,
+    },
+    veganRatingCount: {
+      type: Number,
+      default: 0,
+      min: 0,
+      validate: Number.isInteger,
+    },
+  },
   telephone: {
     type: String,
     required: true,
@@ -59,6 +75,31 @@ const schema = new Schema({
     required: true,
     validate: validator.isURL,
   },
+});
+
+schema.statics.updateVeganRating = function updateVeganRating(
+  _id, restaurantId, ratingTotal, ratingCount,
+) {
+  return Promise.all([
+    Restaurant.updateVeganRating(restaurantId, ratingTotal, ratingCount),
+    this.updateOne(
+      { _id },
+      {
+        $inc: {
+          'reviewMeta.veganRatingTotal': ratingTotal,
+          'reviewMeta.veganRatingCount': ratingCount,
+        },
+      },
+    ),
+  ]);
+};
+
+schema.virtual('reviewMeta.veganRating').get(function getVeganRating() {
+  if (this.reviewMeta.veganRatingCount === 0) {
+    return 0;
+  }
+
+  return this.reviewMeta.veganRatingTotal / this.reviewMeta.veganRatingCount;
 });
 
 module.exports = model('Location', schema);
