7fd4059141f149bc1fef19527342113bbcc180f7
Kevo1ution <kevin26428@gmail.com>
2020-07-01 20:32:41 -0500

    added basic location schema

---
+++ src/models/location.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const { model, Schema } = mongoose;
+
+const schema = new Schema({
+  active: {
+    type: Boolean,
+    required: true,
+  },
+  geo: {
+    latitude: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+        index: '2dsphere',
+      },
+    },
+    longitude: {},
+  },
+  lastScraperRun: {
+    type: Date,
+    required: true,
+  },
+  menuId: {
+    type: mongoose.ObjectId,
+    required: true,
+    ref: 'Menu',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  openingHours: {
+    type: [[Number]],
+    required: true,
+  },
+  priceRange: {
+    type: String,
+  },
+  restaurantId: {
+    type: mongoose.ObjectId,
+    required: true,
+    ref: 'Restaurant',
+  },
+  telephone: {
+    type: String,
+    required: true,
+    validate: validator.isMobilePhone,
+  },
+  url: {
+    type: String,
+    required: true,
+    validate: validator.isURL,
+  },
+});
+
+module.exports = model('Location', schema);
