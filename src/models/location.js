d11cb80e45db7149e6430c74c298bdafe5aa347a
Kevin Tang <kevin26428@gmail.com>
2020-07-02 23:58:34 -0500

    [Gor 39] create location schema (#15)
    * added basic location schema
    
    * added index for restaurantId in location schema
    
    * added reviewMeta property to a location
    
    * made price range required
    
    * removed active field as it is unecessary
    
    * fixed geo field
    
    * added location tests

---
+++ src/models/location.js
@@ -0,0 +1,99 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const Restaurant = require('./restaurant');
+
+const { model, Schema } = mongoose;
+
+const schema = new Schema({
+  geo: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere',
+    },
+  },
+  lastScraperRun: {
+    type: Date,
+    required: true,
+  },
+  menuId: {
+    type: mongoose.ObjectId,
+    required: true,
+    ref: 'Menu',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  openingHours: {
+    type: [[Number]],
+    required: true,
+  },
+  priceRange: {
+    type: String,
+    required: true,
+  },
+  restaurantId: {
+    type: mongoose.ObjectId,
+    required: true,
+    index: true,
+    ref: 'Restaurant',
+  },
+  reviewMeta: {
+    veganRatingTotal: {
+      type: Number,
+      default: 0,
+      min: 0,
+      validate: Number.isInteger,
+    },
+    veganRatingCount: {
+      type: Number,
+      default: 0,
+      min: 0,
+      validate: Number.isInteger,
+    },
+  },
+  telephone: {
+    type: String,
+    required: true,
+    validate: validator.isMobilePhone,
+  },
+  url: {
+    type: String,
+    required: true,
+    validate: validator.isURL,
+  },
+});
+
+schema.statics.updateVeganRating = function updateVeganRating(
+  _id, restaurantId, ratingTotal, ratingCount,
+) {
+  return Promise.all([
+    Restaurant.updateVeganRating(restaurantId, ratingTotal, ratingCount),
+    this.updateOne(
+      { _id },
+      {
+        $inc: {
+          'reviewMeta.veganRatingTotal': ratingTotal,
+          'reviewMeta.veganRatingCount': ratingCount,
+        },
+      },
+    ),
+  ]);
+};
+
+schema.virtual('reviewMeta.veganRating').get(function getVeganRating() {
+  if (this.reviewMeta.veganRatingCount === 0) {
+    return 0;
+  }
+
+  return this.reviewMeta.veganRatingTotal / this.reviewMeta.veganRatingCount;
+});
+
+module.exports = model('Location', schema);
