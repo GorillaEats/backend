b1335bf5a2b627d4d24f16073e338175c9f57b61
Kevo1ution <kevin26428@gmail.com>
2020-07-07 12:38:32 -0500

    adding skeleton for loading routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('app-module-path').addPath(__dirname);
 const setupDB = require('src/loaders/mongoose');
 const logger = require('src/logger');
 const { errorHandler, httpLogger } = require('src/api/middleware');
+const { location } = require('src/api/routes');
 
 const app = express();
 
@@ -11,8 +12,14 @@ setupDB();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(errorHandler);
+// Middleware Pre Routes
 app.use(httpLogger);
+
+// Routes
+app.use(location);
+
+// Middleware Post Routes
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   logger.info(`Listening on Port ${PORT}`);
