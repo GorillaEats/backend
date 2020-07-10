6f22d680c998b7bf74b28c330334547a2a8a7739
Kevo1ution <kevin26428@gmail.com>
2020-07-10 10:57:17 -0500

    only proxyquire once

--- test/util/app.js
+++ test/util/app.js
@@ -3,12 +3,14 @@ const proxyquire = require('proxyquire').noPreserveCache();
 require('./absolutePath');
 const TestDB = require('test/util/mongoose');
 
+const expressApp = proxyquire('index', { 'src/loaders/mongoose': () => {} });
+
 class TestApp {
   async setup() {
     const testDb = new TestDB();
     await testDb.setup({ seed: true });
 
-    this.expressApp = proxyquire('index', { 'src/loaders/mongoose': () => {} });
+    this.expressApp = expressApp;
     this.testDb = testDb;
   }
 
