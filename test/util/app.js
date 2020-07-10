ed78e9a5a6bd740706e0975b77f504d707d98b2a
Kevo1ution <kevin26428@gmail.com>
2020-07-10 10:53:39 -0500

    mock setupdb

--- test/util/app.js
+++ test/util/app.js
@@ -1,4 +1,3 @@
-const sinon = require('sinon');
 const proxyquire = require('proxyquire').noPreserveCache();
 
 require('./absolutePath');
@@ -9,26 +8,12 @@ class TestApp {
     const testDb = new TestDB();
     await testDb.setup({ seed: true });
 
-    const mongoUri = await testDb.mongod.getUri();
-
-    if (process.env.MONGODB_CONNECTION_URL) {
-      this.envStub = sinon.stub(process.env.MONGODB_CONNECTION_URL).value(mongoUri);
-    } else {
-      process.env.MONGODB_CONNECTION_URL = mongoUri;
-    }
-
-    this.expressApp = proxyquire('index', {});
+    this.expressApp = proxyquire('index', { 'src/loaders/mongoose': () => {} });
     this.testDb = testDb;
   }
 
   async cleanup() {
     await this.testDb.cleanup();
-
-    if (this.envStub) {
-      this.envStub.restore();
-    } else {
-      delete process.env.MONGODB_CONNECTION_URL;
-    }
   }
 }
 
