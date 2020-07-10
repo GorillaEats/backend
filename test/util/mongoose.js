0bf4507d8304b561bdac32354c32b07dbed133d3
Kevo1ution <kevin26428@gmail.com>
2020-07-09 19:17:28 -0500

    converted test db to class

--- test/util/mongoose.js
+++ test/util/mongoose.js
@@ -5,33 +5,42 @@ require('./absolutePath');
 const Models = require('src/models');
 const seedData = require('test/util/seedData.json');
 
-async function seedDB() {
-  await Promise.all(Object.keys(Models).map((modelName) => {
-    if (seedData[modelName]) {
-      return Models[modelName].insertMany(seedData[modelName]);
-    }
-    return Promise.resolve();
-  }));
-}
+class TestDB {
+  async seedDB() {
+    const modelNames = Object.keys(Models);
+    const data = {};
+    await Promise.all(modelNames.map(async (modelName) => {
+      if (seedData[modelName]) {
+        const Model = Models[modelName];
+        await Model.insertMany(seedData[modelName]);
+        data[modelName] = await Model.find({});
+      } else {
+        data[modelName] = [];
+      }
+    }));
 
-async function setupMongodbConnection(options = {}) {
-  const mongod = new MongoMemoryServer();
-  const mongoUri = await mongod.getUri();
-  await mongoose.connect(mongoUri, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  if (options.seed) {
-    await seedDB();
+    this.data = data;
   }
 
-  return mongod;
+  async setupMongodbConnection(options = {}) {
+    const mongod = new MongoMemoryServer();
+    const mongoUri = await mongod.getUri();
+    await mongoose.connect(mongoUri, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    if (options.seed) {
+      await this.seedDB();
+    }
+
+    this.mongod = mongod;
+  }
+
+  async closeMongodbConnection() {
+    await this.mongod.stop();
+  }
 }
 
-async function closeMongodbConnection(mongod) {
-  await mongod.stop();
-}
-
-module.exports = { setupMongodbConnection, closeMongodbConnection };
+module.exports = TestDB;
