d82b2a43bcdc7f285880e901640a70af7e99bd19
Kevo1ution <kevin26428@gmail.com>
2020-07-10 00:42:58 -0500

    made test app compatible with mongoose test db

--- test/util/testApp.js
+++ test/util/testApp.js
@@ -2,12 +2,14 @@ const sinon = require('sinon');
 const proxyquire = require('proxyquire').noPreserveCache();
 
 require('./absolutePath');
-const { setupMongodbConnection, closeMongodbConnection } = require('test/util/mongoose');
+const TestDB = require('test/util/mongoose');
 
 class TestApp {
   async setup() {
-    this.mongod = await setupMongodbConnection({ seed: true });
-    const mongoUri = await this.mongod.getUri();
+    const testDb = new TestDB();
+    await testDb.setup({ seed: true });
+
+    const mongoUri = await testDb.mongod.getUri();
 
     if (process.env.MONGODB_CONNECTION_URL) {
       this.envStub = sinon.stub(process.env.MONGODB_CONNECTION_URL).value(mongoUri);
@@ -16,10 +18,11 @@ class TestApp {
     }
 
     this.expressApp = proxyquire('index', {});
+    this.testDb = testDb;
   }
 
   async cleanup() {
-    await closeMongodbConnection(this.mongod);
+    await this.testDb.cleanup();
 
     if (this.envStub) {
       this.envStub.restore();
