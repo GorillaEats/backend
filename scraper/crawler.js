abf74e9b132c274aa30c0b382542523a1573b7b8
Kevo1ution <kevin26428@gmail.com>
2020-07-13 16:14:30 -0500

    added depth limit for scraper

--- scraper/crawler.js
+++ scraper/crawler.js
@@ -1,20 +1,36 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const Apify = require('apify');
 
+const { Location } = require('src/models/index');
+
+const MAX_DEPTH = 4;
+
 async function createCrawler(restaurant) {
   const { url, allow } = restaurant.spider;
   const requestQueue = await Apify.openRequestQueue();
 
-  await requestQueue.addRequest({ url: restaurant.spider.url });
+  await requestQueue.addRequest({
+    url: restaurant.spider.url,
+    userData: {
+      depth: 0,
+    },
+  });
 
   const handlePageFunction = async ({ request, $ }) => {
-    await Apify.utils.enqueueLinks({
-      $,
-      requestQueue,
-      baseUrl: url,
-      pseudoUrls: [new RegExp(allow)],
-      limit: 5,
-    });
+    if (request.userData.depth < MAX_DEPTH) {
+      await Apify.utils.enqueueLinks({
+        $,
+        requestQueue,
+        baseUrl: url,
+        pseudoUrls: [new RegExp(allow)],
+        limit: 5,
+        transformRequestFunction: (oldReq) => {
+          const newReq = oldReq;
+          newReq.userData.depth = request.userData.depth + 1;
+          return newReq;
+        },
+      });
+    }
   };
 
   return new Apify.CheerioCrawler({
