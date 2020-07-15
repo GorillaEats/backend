0e77f0dc1f7751f66da7b373969e6d9f1c63a3ee
Sarah <asarahali00@gmail.com>
2020-07-15 14:15:26 -0500

    add restaurant connection to mongoDB

--- src/api/routes/restaurant.js
+++ src/api/routes/restaurant.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { celebrate, Joi, Segments } = require('celebrate');
 
+const Restaurant = require('src/models/restaurant.js');
+
 const PATH = '/restaurant';
 
 const router = express.Router();
@@ -8,15 +10,21 @@ const router = express.Router();
 router.get('/:id',
   celebrate({
     [Segments.PARAMS]: Joi.object().keys({
-      id: Joi.string().custom((value, helpers) => {
-        if (!value || !String(value).match(/^[0-9a-fA-F]{24}$/)) { return helpers.error('any.invalid'); }
+      id: Joi.string().custom((id, helpers) => {
+        if (!id || !String(id).match(/^[0-9a-fA-F]{24}$/)) { return helpers.error('any.invalid'); }
 
-        return { value };
+        return { id };
       }, 'custom validation'),
     }),
   }),
-  (req, res) => {
-    res.send('works');
+  async (req, res) => {
+    const { id } = req.params.id;
+
+    const restaurant = await Restaurant
+      .findById(id)
+      .lean();
+
+    res.json({ restaurant });
   });
 
 module.exports = {
